0cac7eb766453572ca8afc189cc0e64fea5e640a
helenbrennan <helenbrennan@gmail.com>
2019-08-31 12:17:20 -0600

    updates

--- pageObjects/pageObjects.js
+++ pageObjects/pageObjects.js
@@ -1,3 +1,97 @@
 module.exports = {
     url: ('https://www.nationalmssociety.org'),
+    elements: {
+        signIn: {
+            selector: ('//*[@id="quick-links"]/div[1]/div/a'),
+            locateStrategy: 'xpath'
+        },
+        inYourArea: {
+            selector: ('//*[@id="p_lt_zoneMastheadNavColumn1_SelectedChapterCalloutControl_userControlElem_lnkChapters"]'),
+            locateStrategy: 'xpath'
+        },
+        donateButton: {
+            selector: ('//div[@class="masthead-nav-group masthead-nav-donate"]'),
+            locateStrategy: 'xpath'
+        },
+        searchBar: ('.search-controls'),
+        // searchBar: {
+        //     selector:('//*[@name="p$lt$zoneMastheadNavColumn2$GoogleSearchbox$txtSearchBox"]'),
+        //     locateStrategy: 'xpath'
+        // }
+        content: {
+            selector: ('//div[@class="content"]'),
+            locateStrategy: 'xpath'
+        },
+        popUpBox: ('.fancybox-outer'),
+        popUpBoxClose: ('.fancybox-item'),
+        whatIsMS: {
+            selector: ('//span[@id="What-is-MS-link"]'),
+            locateStrategy: 'xpath'
+        },
+        whatIsMSSubNav: ('#subNavWhatisMS'),
+        symptomsDx: {
+            selector: ('//*[@id="Symptoms-Diagnosis-link"]'),
+            locateStrategy: 'xpath'
+        },
+        treatingMS: {
+            selector: ('//*[@id="Treating-MS-link"]'),
+            locateStrategy: 'xpath'
+        },
+        resourceSupport: {
+            selector: ('//*[@id="Resources-Support-link"]'),
+            locateStrategy: 'xpath'
+        },
+        livingWell: {
+            selector: ('//*[@id="Living-Well-With-MS-link"]'),
+            locateStrategy: 'xpath'
+        },
+        research: {
+            selector: ('//*[@id="Research-link"]'),
+            locateStrategy: 'xpath'
+        },
+        getInvolved: {
+            selector: ('//*[@id="Get-Involved-link]'),
+            locateStrategy: 'xpath'
+        },
+        pageOne: {
+            selector: ('//*[@id="router-navigation"]/li[1]/div/div/div/a[1]'),
+            locateStrategy: 'xpath'
+        },
+        pageTwo: {
+            selector: ('//*[@id="router-navigation"]/li[2]'),
+            locateStrategy: 'xpath'
+        },
+        pageThree: {
+            selector: ('//*[@id="router-navigation"]/li[3]'),
+            locateStrategy: 'xpath'
+        },
+        pageFour: {
+            selector: ('//*[@id="router-navigation"]/li[4]'),
+            locateStrategy: 'xpath'
+        },
+        pageFive: {
+            selector: ('//*[@id="router-navigation"]/li[5]'),
+            locateStrategy: 'xpath'
+        },
+        pageSix: {
+            selector: ('//*[@id="router-navigation"]/li[6]'),
+            locateStrategy: 'xpath'
+        },
+        pageSeven: {
+            selector: ('//*[@id="router-navigation"]/li[7]'),
+            locateStrategy: 'xpath'
+        },
+        pageEight: {
+            selector: ('//*[@id="router-navigation"]/li[8]'),
+            locateStrategy: 'xpath'
+        },
+        pageNine: {
+            selector: ('//*[@id="router-navigation"]/li[9]'),
+            locateStrategy: 'xpath'
+        },
+        breadcrumbsBackOne: {
+            selector: ('//*[@class="CMSBreadCrumbsLink"]'),
+            locateStrategy: 'xpath'
+        }
+    }
 }
--- tests/mainTest.js
+++ tests/mainTest.js
@@ -1,17 +1,37 @@
 var pageObjects = {}
 var clickByText = (browser, text) => {
-    browser
+    pageObjects
         .useXpath()
         .click(`//*[text()="${text}"]`)
         .useCss()
 }
 module.exports = {
-    beforeEach: browser => {
+    before: browser => {
         pageObjects = browser.page.pageObjects()
         pageObjects.navigate()
+            .maximizeWindow()
     },
     after: browser => {
         browser.end()
     },
-    
+    'Q4H-101 Internal links - guest users - What is MS?': browser => {
+        pageObjects
+            .moveToElement('@whatIsMS', 20, 20)
+            .waitForElementVisible('@whatIsMSSubNav')
+            .click('@whatIsMS')
+            .waitForElementVisible('@popUpBox')
+            .click('@popUpBoxClose')
+            .expect.element('@content').text.to.contain('What Is MS?').before(8000)
+        pageObjects
+        // I don't know what this means
+        // WebDriverWait wait1 = new WebDriverWait(driver, 10)
+        // WebElement element1 = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath('//*[@id="router-navigation"]/li[1]/div/div/div/a[1]))),
+        // element1.click(),
+        //
+        // this isn't working
+        // .click('@pageOne')
+        // .expect.element('@content').text.to.contain('Definition of MS').before(10000)
+        // pageObjects
+
+}    
 }
