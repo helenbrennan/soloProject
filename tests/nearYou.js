b28955cd9a99b4d2d48d4cfb9ffe0418c126aea3
helenbrennan <helenbrennan@gmail.com>
2019-09-03 12:21:20 -0600

    nearYou test

---
+++ tests/nearYou.js
@@ -0,0 +1,30 @@
+var pageObjects = {}
+var clickByText = (browser, text) => {
+    pageObjects
+        .useXpath()
+        .click(`//*[text()="${text}"]`)
+        .useCss()
+}
+module.exports = {
+    before: browser => {
+        pageObjects = browser.page.pageObjects()
+        pageObjects.navigate()
+            .maximizeWindow()
+    },
+    after: browser => {
+        browser.end()
+    },
+    'Q4H-117 ‘Near You’ events': browser => {
+        pageObjects
+            .click('@inYourArea')
+            .waitForElementVisible('@inYourAreaSearch')
+            .setValue('@inYourAreaSearch', '84010')
+            .click('@inYourAreaSearchButton')
+            .expect.element('@inYourAreaResults').text.to.contain('Utah-Southern Idaho').before(10000)
+        pageObjects
+            .clearValue('@tryAnotherSearch')
+            .setValue('@tryAnotherSearch', '67846')
+            .click('@tryAnotherSearchButton')
+            .expect.element('@inYourAreaResults').text.to.contain('Mid America').before(10000)
+    }
+}
